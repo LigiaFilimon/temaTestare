5fe55fdfc3f305f536d26dbdbc91b7c81a7f6513
LigiaFilimon <pasca_ligia@yahoo.com>
2018-01-24 22:43:14 +0200

    Initial commit

---
+++ src/test/java/org/fasttrackit/SimpleSearch.java
@@ -0,0 +1,61 @@
+package org.fasttrackit;
+
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+/**
+ * Unit test for simple App.
+ */
+
+public class SimpleSearch {
+
+
+    @Test
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public void SimpleSearch() {
+        System.setProperty("webdriver.chrome.driver",
+                "/Users/Darius/Documents/webdrivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://fasttrackit.org/selenium-test/");
+
+        driver.findElement(By.id("search")).sendKeys("vase"+ Keys.ENTER);
+        driver.quit();
+
+    }
+
+    @Test
+    public void SimpleSearchByName() {
+        System.setProperty("webdriver.chrome.driver",
+                "/Users/Darius/Documents/webdrivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://fasttrackit.org/selenium-test/");
+
+        driver.findElement(By.name("q")).sendKeys("vase"+Keys.ENTER);
+        driver.quit();
+
+    }
+
+    @Test
+
+    public void SimpleSearchByClass(){
+        System.setProperty("webdriver.chrome.driver",
+                "/Users/Darius/Documents/webdrivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://fasttrackit.org/selenium-test/");
+        driver.findElement(By.className("input-text required-entry")).sendKeys("a tale of two cities"+Keys.ENTER);
+    }
+
+}
+
+
+
+
